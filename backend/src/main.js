aad865ebb2a44e7f5c491276cce7dcaf9dbf39f6
ainacteru <gbenawijr@gmail.com>
2026-01-02 10:17:30 -0600

    changed workflow

--- backend/src/main.js
+++ backend/src/main.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.end(ew.bleh()+ '\n');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
