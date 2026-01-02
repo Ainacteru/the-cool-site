36344813b695c323999a79da832c4fe9a77e6d4d
ainacteru <gbenawijr@gmail.com>
2026-01-01 22:05:31 -0600

    more

---
+++ src/ew.js
@@ -0,0 +1,4 @@
+
+export function bleh() {
+    return 'bleh';
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 const http = require('http');
+const ew = require('./ew.js');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  res.end('Hello, World!\n');
+  res.end(ew.bleh()+ '\n');
 });
 
 const PORT = 3000;
